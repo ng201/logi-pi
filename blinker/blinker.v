66f99230d03f281b50f090b1f461187187ee1a3d
ng201 <gnemeth.mail@gmail.com>
2018-03-29 07:58:04 +0200

    Update blinker.v

--- blinker/blinker.v
+++ blinker/blinker.v
@@ -1,16 +1,21 @@
 `timescale 1ns / 1ps
 
-module blinker(input clk, input rst, input sclk, output ld1, output ld2);
+module blinker(
+    input clk,
+	 input rst,
+	 input sclk,	 
+    output ld1,
+    output ld2
+);
 
 reg [1:0] counter;
 
 always @(posedge clk)
-
     if(rst)
-	      counter <= 0;
-    else 
-	      if(sclk)
-		        counter <= counter + 1;
+	     counter <= 0;
+	 else 
+	     if(sclk)
+		      counter <= counter + 1;
 
 
 assign ld1 = counter[1];
